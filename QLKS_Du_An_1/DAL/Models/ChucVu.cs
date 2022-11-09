7cd7f973518559638f7d26b505a247bce8ae295f
PhamDuong39 <phamanhduong30092003@gmail.com>
2022-11-09 13:53:12 +0700

    UpdateDB

---
+++ QLKS_Du_An_1/DAL/Models/ChucVu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class ChucVu
+    {
+        public Guid ID { get; set; }
+        public string MaCV { get; set; }
+        public string TenCV { get; set; }
+        public virtual ICollection<NhanVien> NhanViens { get; set; }
+
+    }
+}
---
+++ QLKS_Du_An_1/DAL/Models/QLKS_DA1_DbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class QLKS_DA1_DbContext : DbContext
+    {
+        public QLKS_DA1_DbContext()
+        {
+
+        }
+
+        public QLKS_DA1_DbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // thuc hien rang buoc ket noi
+            base.OnConfiguring(optionsBuilder
+                .UseSqlServer("Data Source=DESKTOP-0GA19F5\\SQLEXPRESS;Initial Catalog=test2;Persist Security Info=True;User ID=duongpaph28223;Password=123456 "));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // apply config cho cac model
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
