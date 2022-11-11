45aa4418a82ba68b1b6e8ed92dbda1a411e2a642
Portargard <portargard@gmail.com>
2022-11-11 22:04:19 +0700

    them dbset cho tung bang(de code sau nay giong voi db first cho quen tay thich thi xoa di cx dc)

--- QLKS_Du_An_1/DAL/Models/QLKS_DA1_DbContext.cs
+++ QLKS_Du_An_1/DAL/Models/QLKS_DA1_DbContext.cs
@@ -24,7 +24,7 @@ namespace DAL.Models
         {
             // thuc hien rang buoc ket noi
             base.OnConfiguring(optionsBuilder
-                .UseSqlServer("Data Source=DESKTOP-0GA19F5\\SQLEXPRESS;Initial Catalog=QLKS_DuAn1_Lan2;Persist Security Info=True;User ID=duongpaph28223;Password=123456"));
+                .UseSqlServer("Data Source=DESKTOP-OKDB069;Initial Catalog=QLKS_Du_An_1;Integrated Security=True"));
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -32,5 +32,19 @@ namespace DAL.Models
             // apply config cho cac model
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
+        public virtual DbSet<ChiTietPhieuThue> ChiTietPhieuThues { get; set; }
+        public virtual DbSet<ChiTietTienNghi> ChiTietTienNghis { get; set; }
+        public virtual DbSet<ChucVu> ChucVus { get; set; }
+        public virtual DbSet<HoaDon> HoaDons { get; set; }
+        public virtual DbSet<DichVu> DichVus { get; set; }
+        public virtual DbSet<HoaDonChiTiet> HoaDonChiTiets { get; set; }
+        public virtual DbSet<KhachHang> KhachHangs { get; set; }
+        public virtual DbSet<LoaiDichVu> LoaiDichVus { get; set; }
+        public virtual DbSet<LoaiPhong> LoaiPhongs { get; set; }
+        public virtual DbSet<LoaiTienNghi> LoaiTienNghis { get; set; }
+        public virtual DbSet<NhanVien> NhanViens { get; set; }
+        public virtual DbSet<PhieuThue> PhieuThues { get; set; }
+        public virtual DbSet<Phong> Phongs { get; set; }
+        public virtual DbSet<TaiKhoan> TaiKhoans { get; set; }
     }
 }
