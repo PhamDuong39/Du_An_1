7a1bb21355bec24408bbbc252a75c67ecd8ebbc4
PhamDuong39 <phamanhduong30092003@gmail.com>
2022-11-16 13:44:12 +0700

    Restore ALl

---
+++ QLKS_Du_An_1/BUS/IServices/IHoaDonService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BUS.ViewModels;
+
+namespace BUS.IServices
+{
+    public interface IHoaDonService
+    {
+        public string Add(HoaDonView obj);
+        public string Update(HoaDonView obj);
+        public string Remove(HoaDonView obj);
+        public List<HoaDonView> GetAll();
+        public List<HoaDonView> Search(string keyWord);
+    }
+}
---
+++ QLKS_Du_An_1/BUS/Services/HoaDonService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BUS.IServices;
+using BUS.ViewModels;
+using DAL.IRepositories;
+using DAL.Repositories;
+using BUS.Ultilities;
+
+namespace BUS.Services
+{
+    public class HoaDonService : IHoaDonService
+    {
+        private IHoaDonRepository _hoaDonRepository;
+        private Validations _validations;
+        public HoaDonService()
+        {
+            _hoaDonRepository = new HoaDonRepository();
+            _validations = new Validations();
+        }
+        private string Validate(HoaDonView obj)
+        {
+            if (_validations.CheckRong(obj.MaHD) == false) return "Nhập đầy đủ dữ liệu";
+            if (_validations.CheckRong(obj.TenKH) == false) return "Nhập đầy đủ dữ liệu";
+            if (_validations.CheckRong(obj.TenNV) == false) return "Nhập đầy đủ dữ liệu";
+            if (_validations.CheckRong(obj.MaHD) == false) return "Nhập đầy đủ dữ liệu";
+            return string.Empty;
+        }
+        public string Add(HoaDonView obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<HoaDonView> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Remove(HoaDonView obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(HoaDonView obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<HoaDonView> Search(string keyWord)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
