7af78d7848b66a17078a50155b2e3b3e7e346f33
toannk <toannkph27888@fpt.edu.vn>
2022-11-16 12:24:43 +0700

    Sua chinh ta

--- QLKS_Du_An_1/DAL/Models/QLKS_DA1_DbContext.cs
+++ QLKS_Du_An_1/DAL/Models/QLKS_DA1_DbContext.cs
@@ -24,7 +24,7 @@ namespace DAL.Models
         {
             // thuc hien rang buoc ket noi
             base.OnConfiguring(optionsBuilder
-                .UseSqlServer("Data Source=DESKTOP-0GA19F5\\SQLEXPRESS;Initial Catalog=QLKS_DuAn1_Lan2;Persist Security Info=True;User ID=duongpaph28223;Password=123456"));
+                .UseSqlServer("Data Source=DESKTOP-QQ18T65\\SQLEXPRESS;Initial Catalog=DuAn1;Integrated Security=True"));
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
