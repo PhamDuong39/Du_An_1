44169d36a0c7e1a966a963d34f62060dbb48f15d
Hiepdepzainhat <hiepdepzai2k3@gmail.com>
2022-11-18 14:57:00 +0700

    abc

--- QLKS_Du_An_1/DAL/Models/QLKS_DA1_DbContext.cs
+++ QLKS_Du_An_1/DAL/Models/QLKS_DA1_DbContext.cs
@@ -25,11 +25,8 @@ namespace DAL.Models
             // thuc hien rang buoc ket noi
             base.OnConfiguring(optionsBuilder
 
-<<<<<<< Updated upstream
                 .UseSqlServer("Data Source=LAPTOP-DAV1LO0Q\\SQLEXPRESS;Initial Catalog=QLKS_DuAn1_Lan2;Persist Security Info=True;User ID=hiepnt;Password=hiepa8k57tn"));
-=======
-                .UseSqlServer("Data Source=LAPTOP-DAV1LO0Q\\SQLEXPRESS;Initial Catalog=QLKS_DuAn1_Lan2;Persist Security Info=True;User ID=hiepnt;Password=hiepa8k57tn "));
->>>>>>> Stashed changes
+
 
 
         }
