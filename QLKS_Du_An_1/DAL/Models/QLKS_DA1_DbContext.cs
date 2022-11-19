e482dff30ee04b9308b642629fd3b241b0949ff4
Phuongmvph27860 <phuonglay90cc.cc@gmail.com>
2022-11-19 18:43:29 +0700

    add FormQLLoaiPhong, FromBtnSuaLoaiPhong, FormBtnThemLoaiPhong

--- QLKS_Du_An_1/DAL/Models/QLKS_DA1_DbContext.cs
+++ QLKS_Du_An_1/DAL/Models/QLKS_DA1_DbContext.cs
@@ -25,8 +25,7 @@ namespace DAL.Models
             // thuc hien rang buoc ket noi
             base.OnConfiguring(optionsBuilder
 
-                .UseSqlServer("Data Source=LAPTOP-07LH20M4\\SQLEXPRESS02;Initial Catalog=QLKS_DuAn1_Lan2;Persist Security Info=True;User ID=DBSET;Password=123"));
-
+                .UseSqlServer("Data Source=DESKTOP-H6UFHDN\\SQLEXPRESS;Initial Catalog=QLKS_DuAn1;Persist Security Info=True;User ID=phuongmvph27860;Password=123"));
 
         }
 
--- QLKS_Du_An_1/GUI/View/AddControls/FrmBtnThemLoaiPhong.cs
+++ QLKS_Du_An_1/GUI/View/AddControls/FrmBtnThemLoaiPhong.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BUS.IServices;
+using BUS.Services;
+using BUS.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +15,32 @@ namespace GUI.View.AddControls
 {
     public partial class FrmBtnThemLoaiPhong : Form
     {
+        private IQLLoaiPhongService _iqlLoaiPhongService;
         public FrmBtnThemLoaiPhong()
         {
             InitializeComponent();
+            _iqlLoaiPhongService = new ILoaiPhongService();
+        }
+
+        private void btn_ThemLoaiPhong_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có muốn thêm loại phòng này không?", "Thông báo", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                var lpv = new LoaiPhongView()
+                {
+                    ID = Guid.NewGuid(),
+                    TenLoaiPhong = tb_TenLoaiPhong.Text,
+                    MaLoaiPhong = tb_MaLoaiPhong.Text,
+                    SoGiuong = int.Parse(tb_SoGiuong.Text),
+                    GiaNgay = int.Parse(tb_GiaNgay.Text)
+                };
+                MessageBox.Show(_iqlLoaiPhongService.Add(lpv));
+            }
+            else
+            {
+                MessageBox.Show("Bạn đã không thên loại phòng này");
+            }
         }
     }
 }
