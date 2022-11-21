25f591b08475683205ca1b695543a54251934a3e
PhamDuong39 <phamanhduong30092003@gmail.com>
2022-11-21 11:56:44 +0700

    Add CTTN Update QLPhong

--- QLKS_Du_An_1/GUI/View/AddControls/FrmBtnEditDetailPhong.cs
+++ QLKS_Du_An_1/GUI/View/AddControls/FrmBtnEditDetailPhong.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BUS.IServices;
+using BUS.Services;
+using GUI.View.UserControls;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +15,70 @@ namespace GUI.View.AddControls
 {
     public partial class FrmBtnEditDetailPhong : Form
     {
+        public string MaPhong { get; set; }
+        public Guid IdRoomSelected { get; set; }
+
+        private IQLChiTietTienNghiService _iqlCTTNService;
+
         public FrmBtnEditDetailPhong()
         {
             InitializeComponent();
+            _iqlCTTNService = new QLChiTietTienNghiService();
+
+            LoadDataCTTNTrong();
+            LoadDataCTTNPhong();
+        }
+
+        private void LoadDataCTTNTrong()
+        {
+            dtg_DanhSachCTTNTrong.ColumnCount = 4;
+            dtg_DanhSachCTTNTrong.Rows.Clear();
+            dtg_DanhSachCTTNTrong.Columns[0].Name = "ID CTTN";
+            dtg_DanhSachCTTNTrong.Columns[0].Visible = false;
+            dtg_DanhSachCTTNTrong.Columns[1].Name = "Ma CTTN";
+            dtg_DanhSachCTTNTrong.Columns[2].Name = "Ten CTTN";
+            dtg_DanhSachCTTNTrong.Columns[3].Name = "Ten Loai TN";
+
+            var lstCTTNTrong = _iqlCTTNService.GetEmptyCTTN();
+        
+            foreach (var item in lstCTTNTrong)
+            {
+                dtg_DanhSachCTTNTrong.Rows.Add(item.ID, item.MaCTTienNghi, item.TenCTTienNghi, item.TenLoaiTienNghi);
+            }
+        }
+
+        private void LoadDataCTTNPhong()
+        {
+            dtg_DanhSachCTTNPhong.ColumnCount = 6;
+            dtg_DanhSachCTTNPhong.Rows.Clear();
+            dtg_DanhSachCTTNPhong.Columns[0].Name = "ID CTTN";
+            dtg_DanhSachCTTNPhong.Columns[0].Visible = false;
+            dtg_DanhSachCTTNPhong.Columns[1].Name = "Mã CTTN";
+            dtg_DanhSachCTTNPhong.Columns[2].Name = "Tên CTTN";
+            dtg_DanhSachCTTNPhong.Columns[3].Name = "Tên Loại TN";
+            dtg_DanhSachCTTNPhong.Columns[4].Name = "ID Phòng";
+            dtg_DanhSachCTTNPhong.Columns[4].Visible = false;
+            dtg_DanhSachCTTNPhong.Columns[5].Name = "Mã phòng";
+
+            //var lstCTTNPhong = _iqlCTTNService.GetListCTTNRoom(Guid.Parse("86393AF9-2C4F-43C5-861C-1605E3B96938"));
+            //var lstCTTNPhong = _iqlCTTNService.GetListCTTNRoom(IDPhong);
+            
+            
+
+            var lstCTTNPhong = _iqlCTTNService.GetListCTTNRoom(Guid.Parse("86393AF9-2C4F-43C5-861C-1605E3B96938"));
+  
+            foreach (var item in lstCTTNPhong)
+            {
+                dtg_DanhSachCTTNPhong.Rows.Add(item.ID, item.MaCTTienNghi, item.TenCTTienNghi, item.TenLoaiTienNghi, item.IdPhong, item.MaPhong);
+            }
+           
+        }
+
+        private void FrmBtnEditDetailPhong_Load(object sender, EventArgs e)
+        {
+            lb_TenPhongCTTN.Text = MaPhong;
+            //lb_TenPhongCTTN.Text = "" + IDPhong;           
+            
         }
     }
 }
--- QLKS_Du_An_1/GUI/View/AddControls/FrmBtnSuaTienNghi.Designer.cs
+++ QLKS_Du_An_1/GUI/View/AddControls/FrmBtnSuaTienNghi.Designer.cs
@@ -28,12 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.cbb_MaPhong = new System.Windows.Forms.ComboBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.cbb_TenLoaiTienNghi = new System.Windows.Forms.ComboBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tb_TenCTTNSua = new System.Windows.Forms.TextBox();
+            this.tb_MaCTTNSua = new System.Windows.Forms.TextBox();
+            this.btn_SuaCTTienNghi = new System.Windows.Forms.Button();
+            this.btn_HuySuaCTTienNghi = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.label1.Location = new System.Drawing.Point(160, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 32);
+            this.label1.TabIndex = 43;
+            this.label1.Text = "Sửa Chi Tiết Tiện Nghi";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightGray;
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Controls.Add(this.cbb_MaPhong);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.cbb_TenLoaiTienNghi);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.tb_TenCTTNSua);
+            this.panel1.Controls.Add(this.tb_MaCTTNSua);
+            this.panel1.Location = new System.Drawing.Point(119, 80);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(349, 247);
+            this.panel1.TabIndex = 42;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::GUI.Properties.Resources._007_user_1;
+            this.pictureBox4.Location = new System.Drawing.Point(57, 171);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 28;
+            this.pictureBox4.TabStop = false;
+            // 
+            // cbb_MaPhong
+            // 
+            this.cbb_MaPhong.FormattingEnabled = true;
+            this.cbb_MaPhong.Location = new System.Drawing.Point(102, 172);
+            this.cbb_MaPhong.Name = "cbb_MaPhong";
+            this.cbb_MaPhong.Size = new System.Drawing.Size(191, 23);
+            this.cbb_MaPhong.TabIndex = 27;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::GUI.Properties.Resources._007_user_1;
+            this.pictureBox2.Location = new System.Drawing.Point(57, 124);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 26;
+            this.pictureBox2.TabStop = false;
+            // 
+            // cbb_TenLoaiTienNghi
+            // 
+            this.cbb_TenLoaiTienNghi.FormattingEnabled = true;
+            this.cbb_TenLoaiTienNghi.Location = new System.Drawing.Point(102, 125);
+            this.cbb_TenLoaiTienNghi.Name = "cbb_TenLoaiTienNghi";
+            this.cbb_TenLoaiTienNghi.Size = new System.Drawing.Size(191, 23);
+            this.cbb_TenLoaiTienNghi.TabIndex = 25;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::GUI.Properties.Resources._007_user_1;
+            this.pictureBox3.Location = new System.Drawing.Point(57, 75);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 24;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::GUI.Properties.Resources._007_user_1;
+            this.pictureBox1.Location = new System.Drawing.Point(57, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 23;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tb_TenCTTNSua
+            // 
+            this.tb_TenCTTNSua.Location = new System.Drawing.Point(102, 76);
+            this.tb_TenCTTNSua.Name = "tb_TenCTTNSua";
+            this.tb_TenCTTNSua.PlaceholderText = "Nhập Tên CT Tiện Nghi";
+            this.tb_TenCTTNSua.Size = new System.Drawing.Size(191, 23);
+            this.tb_TenCTTNSua.TabIndex = 21;
+            // 
+            // tb_MaCTTNSua
+            // 
+            this.tb_MaCTTNSua.Location = new System.Drawing.Point(102, 22);
+            this.tb_MaCTTNSua.Name = "tb_MaCTTNSua";
+            this.tb_MaCTTNSua.PlaceholderText = "Nhập mã CT Tiện Nghi";
+            this.tb_MaCTTNSua.Size = new System.Drawing.Size(191, 23);
+            this.tb_MaCTTNSua.TabIndex = 17;
+            // 
+            // btn_SuaCTTienNghi
+            // 
+            this.btn_SuaCTTienNghi.BackColor = System.Drawing.Color.SeaGreen;
+            this.btn_SuaCTTienNghi.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_SuaCTTienNghi.FlatAppearance.BorderSize = 0;
+            this.btn_SuaCTTienNghi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_SuaCTTienNghi.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_SuaCTTienNghi.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_SuaCTTienNghi.Location = new System.Drawing.Point(294, 361);
+            this.btn_SuaCTTienNghi.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_SuaCTTienNghi.Name = "btn_SuaCTTienNghi";
+            this.btn_SuaCTTienNghi.Size = new System.Drawing.Size(96, 30);
+            this.btn_SuaCTTienNghi.TabIndex = 44;
+            this.btn_SuaCTTienNghi.Text = "Cập nhật";
+            this.btn_SuaCTTienNghi.UseVisualStyleBackColor = false;
+            this.btn_SuaCTTienNghi.Click += new System.EventHandler(this.btn_SuaCTTienNghi_Click);
+            // 
+            // btn_HuySuaCTTienNghi
+            // 
+            this.btn_HuySuaCTTienNghi.BackColor = System.Drawing.Color.SeaGreen;
+            this.btn_HuySuaCTTienNghi.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_HuySuaCTTienNghi.FlatAppearance.BorderSize = 0;
+            this.btn_HuySuaCTTienNghi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_HuySuaCTTienNghi.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_HuySuaCTTienNghi.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_HuySuaCTTienNghi.Location = new System.Drawing.Point(416, 361);
+            this.btn_HuySuaCTTienNghi.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_HuySuaCTTienNghi.Name = "btn_HuySuaCTTienNghi";
+            this.btn_HuySuaCTTienNghi.Size = new System.Drawing.Size(96, 30);
+            this.btn_HuySuaCTTienNghi.TabIndex = 45;
+            this.btn_HuySuaCTTienNghi.Text = "Hủy";
+            this.btn_HuySuaCTTienNghi.UseVisualStyleBackColor = false;
+            this.btn_HuySuaCTTienNghi.Click += new System.EventHandler(this.btn_HuySuaCTTienNghi_Click);
+            // 
+            // FrmBtnSuaTienNghi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(585, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn_SuaCTTienNghi);
+            this.Controls.Add(this.btn_HuySuaCTTienNghi);
+            this.Name = "FrmBtnSuaTienNghi";
             this.Text = "FrmBtnSuaTienNghi";
+            this.Load += new System.EventHandler(this.FrmBtnSuaTienNghi_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Panel panel1;
+        private PictureBox pictureBox4;
+        private ComboBox cbb_MaPhong;
+        private PictureBox pictureBox2;
+        private ComboBox cbb_TenLoaiTienNghi;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox1;
+        private TextBox tb_TenCTTNSua;
+        private TextBox tb_MaCTTNSua;
+        private Button btn_SuaCTTienNghi;
+        private Button btn_HuySuaCTTienNghi;
     }
 }
