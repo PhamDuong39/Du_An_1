0a2513645ffed0c4f6943f187be44a5092b2c8dc
Hiepdepzainhat <hiepdepzai2k3@gmail.com>
2022-11-28 11:46:56 +0700

    tk

--- QLKS_Du_An_1/GUI/View/UserControls/FrmThongKe.Designer.cs
+++ QLKS_Du_An_1/GUI/View/UserControls/FrmThongKe.Designer.cs
@@ -28,20 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.sqlCommand1 = new Microsoft.Data.SqlClient.SqlCommand();
+            this.sqlCommand2 = new Microsoft.Data.SqlClient.SqlCommand();
+            this.LiveChart_ThongKe = new LiveCharts.WinForms.CartesianChart();
+            this.dtg_thongke = new System.Windows.Forms.DataGridView();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_thongke)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // sqlCommand1
+            // 
+            this.sqlCommand1.CommandTimeout = 30;
+            this.sqlCommand1.Connection = null;
+            this.sqlCommand1.Notification = null;
+            this.sqlCommand1.Transaction = null;
+            // 
+            // sqlCommand2
+            // 
+            this.sqlCommand2.CommandTimeout = 30;
+            this.sqlCommand2.Connection = null;
+            this.sqlCommand2.Notification = null;
+            this.sqlCommand2.Transaction = null;
+            // 
+            // LiveChart_ThongKe
+            // 
+            this.LiveChart_ThongKe.Location = new System.Drawing.Point(75, 43);
+            this.LiveChart_ThongKe.Name = "LiveChart_ThongKe";
+            this.LiveChart_ThongKe.Size = new System.Drawing.Size(1004, 337);
+            this.LiveChart_ThongKe.TabIndex = 0;
+            this.LiveChart_ThongKe.Text = "cartesianChart1";
+            // 
+            // dtg_thongke
+            // 
+            this.dtg_thongke.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtg_thongke.Location = new System.Drawing.Point(61, 524);
+            this.dtg_thongke.Name = "dtg_thongke";
+            this.dtg_thongke.RowHeadersWidth = 51;
+            this.dtg_thongke.RowTemplate.Height = 29;
+            this.dtg_thongke.Size = new System.Drawing.Size(548, 299);
+            this.dtg_thongke.TabIndex = 1;
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.CurrentChanged += new System.EventHandler(this.bindingSource1_CurrentChanged);
             // 
             // FrmThongKe
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
-            this.ClientSize = new System.Drawing.Size(998, 638);
+            this.ClientSize = new System.Drawing.Size(1141, 851);
+            this.Controls.Add(this.dtg_thongke);
+            this.Controls.Add(this.LiveChart_ThongKe);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "FrmThongKe";
             this.Text = "FrmThongKe";
+            this.Load += new System.EventHandler(this.FrmThongKe_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_thongke)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Microsoft.Data.SqlClient.SqlCommand sqlCommand1;
+        private Microsoft.Data.SqlClient.SqlCommand sqlCommand2;
+        private LiveCharts.WinForms.CartesianChart LiveChart_ThongKe;
+        private DataGridView dtg_thongke;
+        private BindingSource bindingSource1;
     }
 }
--- QLKS_Du_An_1/GUI/View/UserControls/FrmThongKe.cs
+++ QLKS_Du_An_1/GUI/View/UserControls/FrmThongKe.cs
@@ -7,14 +7,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using BUS.IServices;
+using BUS.Services;
+using BUS.ViewModels;
 namespace GUI.View.UserControls
 {
     public partial class FrmThongKe : Form
     {
+        private IQLHoaDonChiTietView _iqlHoaDonCT;
+        private IHoaDonService hoaDonService;
+        public int Month;
+        public Guid IdHDSelect { get; set; }
         public FrmThongKe()
         {
             InitializeComponent();
+            _iqlHoaDonCT = new QLHoaDonChiTietView();
+            hoaDonService = new HoaDonService();
+            LoadDAta();
+        }
+        private void LoadDAta()
+        {
+            int stt = 1;
+            dtg_thongke.Rows.Clear();
+            dtg_thongke.ColumnCount = 4;
+            dtg_thongke.Columns[0].Name = "STT";
+            dtg_thongke.Columns[1].Name = "Tháng";
+            dtg_thongke.Columns[2].Name = "Năm";
+            dtg_thongke.Columns[3].Name = "Tổng Doanh Thu";
+            
+                for(int i = 1; i<= 12; i++)
+                {
+                    dtg_thongke.Rows.Add(i,i,2022,TongTienThang());
+                }
+            
+        }
+
+        private void FrmThongKe_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private int TongTienThang()
+        {
+            int tongtien = 0;
+            
+            
+            for(int month = 0; month <= 12; month++)
+            {
+                var lst = hoaDonService.GetAll().Where(c => c.NgayTaoHD.Month == month);
+                foreach (var t in lst)
+                {
+                    tongtien = tongtien + t.TongTien;
+                }
+                
+            }
+            return tongtien;
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
