0947d5f4776d1c0912a10020f16d36a374eb02bc
Hiepdepzainhat <hiepdepzai2k3@gmail.com>
2022-12-07 13:17:33 +0700

    Hiep add new Branch

--- QLKS_Du_An_1/GUI/View/UserControls/FrmThongKe.cs
+++ QLKS_Du_An_1/GUI/View/UserControls/FrmThongKe.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using BUS.IServices;
+using BUS.Services;
+using BUS.ViewModels;
 namespace GUI.View.UserControls
 {
     public partial class FrmThongKe : Form
     {
+        private IQLHoaDonChiTietView _qlHoadon;
         public FrmThongKe()
         {
             InitializeComponent();
+            _qlHoadon = new QLHoaDonChiTietView();
         }
+        
+
     }
 }
